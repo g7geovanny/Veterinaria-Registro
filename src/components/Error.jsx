d90c714b694ff14161c575d1d5396b43ae623b3a
g7geovanny <sgmichel94@gmail.com>
2022-12-28 19:45:02 -0600

    Primer-commit

---
+++ src/components/Error.jsx
@@ -0,0 +1,17 @@
+
+
+const Error = ({mensaje}) => {
+
+
+  return (
+   
+   
+        <div className=" bg-red-800 text-white text-center p-4 uppercase font-bold mb-3 ">
+          <p>{mensaje}</p>
+        </div>
+
+
+  )
+}
+
+export default Error
